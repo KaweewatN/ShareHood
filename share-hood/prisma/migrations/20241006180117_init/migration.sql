5535a20ef00b9c5ee5ace2019598bc9e0884dbe5
TK <klarrrr17@gmail.com>
2024-10-07 01:11:56 +0700

    feat: connect supabase

---
+++ share-hood/prisma/migrations/20241006180117_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('Developer', 'Admin', 'Renter', 'Customer');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "user_id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
