67d6b8a77b8618b97255dfd6505be0adbe87d5ef
TK <klarrrr17@gmail.com>
2024-10-27 23:48:51 +0700

    feat: add item and noti api

---
+++ share-hood/app/api/items/[id]/route.ts
@@ -0,0 +1,75 @@
+import {NextResponse} from 'next/server';
+import {StatusCode} from 'constants/statusCode';
+import {sql} from '@vercel/postgres';
+import {ItemType} from '../../../../types/api/apiType';
+
+export async function GET(request: Request) {
+  try {
+    const {pathname} = new URL(request.url);
+    const itemIDParam = pathname.split('/').pop();
+    const data = await sql`
+      SELECT * FROM "Item" WHERE "itemID" = ${itemIDParam};
+    `;
+    return NextResponse.json(data.rows, {status: StatusCode.SUCCESS_OK.code});
+  } catch (error: unknown) {
+    return NextResponse.json({error: error as string}, {status: StatusCode.ERROR_NOT_FOUND.code});
+  }
+}
+
+export async function PUT(request: Request) {
+  try {
+    const {pathname} = new URL(request.url);
+    const itemIDParam = pathname.split('/').pop();
+
+    const {
+      userID,
+      itemName,
+      itemDescription,
+      itemPrice,
+      itemQuantity,
+      itemStatus,
+      category,
+      itemReturnDuration,
+      dateAdded,
+      pickupLocation,
+    }: ItemType = await request.json();
+
+    await sql`
+      UPDATE "Item"
+      SET
+        "userID" = ${userID},
+        "itemName" = ${itemName},
+        "itemDescription" = ${itemDescription},
+        "itemPrice" = ${itemPrice},
+        "itemQuantity" = ${itemQuantity},
+        "itemStatus" = ${itemStatus},
+        "category" = ${category},
+        "itemReturnDuration" = ${itemReturnDuration},
+        "dateAdded" = ${dateAdded},
+        "pickupLocation" = ${pickupLocation}
+      WHERE "itemID" = ${itemIDParam}
+    `;
+    return NextResponse.json(`Update Item: ${itemIDParam} Succeed`, {
+      status: StatusCode.SUCCESS_OK.code,
+    });
+  } catch (error: unknown) {
+    return NextResponse.json({error: error as string}, {status: StatusCode.ERROR_BAD_REQUEST.code});
+  }
+}
+
+export async function DELETE(request: Request) {
+  try {
+    const {pathname} = new URL(request.url);
+    const itemIDParam = pathname.split('/').pop();
+
+    await sql`
+      DELETE FROM "Item"
+      WHERE "itemID" = ${itemIDParam}
+    `;
+    return NextResponse.json(`Delete Item: ${itemIDParam} Succeed`, {
+      status: StatusCode.SUCCESS_NO_CONTENT.code,
+    });
+  } catch (error: unknown) {
+    return NextResponse.json({error: error as string}, {status: StatusCode.ERROR_BAD_REQUEST.code});
+  }
+}
--- share-hood/app/api/notifications/route.ts
+++ share-hood/app/api/notifications/route.ts
@@ -3,13 +3,13 @@ import {NextResponse} from 'next/server';
 
 // Files
 import {StatusCode} from 'constants/statusCode';
-import {UserType} from '../../../types/api/apiType';
+import {NotificationType} from '../../../types/api/apiType';
 import {QueryAll} from '@service/db/dbService';
 import {sql} from '@vercel/postgres';
 
 export async function GET() {
   try {
-    const data = await QueryAll<UserType>('User');
+    const data = await QueryAll<NotificationType>('Notification');
     return NextResponse.json(data, {status: StatusCode.SUCCESS_OK.code});
   } catch (error: unknown) {
     return NextResponse.json({error: error as string}, {status: StatusCode.ERROR_NOT_FOUND.code});
@@ -18,25 +18,24 @@ export async function GET() {
 
 export async function POST(request: Request) {
   try {
-    const {userID, password, email, role, verified}: UserType = await request.json();
+    const {notificationID, title, description, status}: NotificationType = await request.json();
 
-    const result = await sql`
-    INSERT INTO "Item" (
-      "userID",
-      "password",
-      "email",
-      "role",
-      "verified"
-    ) VALUES (
-      ${userID},
-      ${password},
-      ${email},
-      ${role},
-      ${verified}
-    )
-    RETURNING *;
-  `;
-    return NextResponse.json(result.rows[0], {status: StatusCode.SUCCESS_OK.code});
+    await sql<NotificationType[]>`
+      INSERT INTO "Notification" (
+        "notificationID",
+        "title",
+        "description",
+        "dateCreated",
+        "status"
+      ) VALUES (
+        ${notificationID},
+        ${title},
+        ${description},
+        NOW(),
+        ${status}
+      )
+    `;
+    return NextResponse.json('Insert Notiication succeed', {status: StatusCode.SUCCESS_OK.code});
   } catch (error: unknown) {
     return NextResponse.json({error: error as string}, {status: StatusCode.ERROR_BAD_REQUEST.code});
   }
