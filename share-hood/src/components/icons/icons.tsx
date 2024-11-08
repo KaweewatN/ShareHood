ffd4d2517514923478a9707556f3f582dcc81f79
TK <klarrrr17@gmail.com>
2024-11-09 04:31:20 +0700

    feat: add menuBar

--- share-hood/src/components/icons/icons.tsx
+++ share-hood/src/components/icons/icons.tsx
@@ -11,9 +11,13 @@ import {
   FaGifts,
   FaSuitcase,
   FaCamera,
+  FaHome,
+  FaRegHeart,
+  FaHeart,
 } from "react-icons/fa";
 import {GiConverseShoe} from "react-icons/gi";
-import {PiHighHeelFill} from "react-icons/pi";
+import {PiHighHeelFill, PiListMagnifyingGlass} from "react-icons/pi";
+import {RiNotification3Line, RiNotification3Fill} from "react-icons/ri";
 
 const Icons = {
   headphones: (className?: string) => <FaHeadphones className={className} />,
@@ -33,6 +37,12 @@ const Icons = {
   Suitcase: (className?: string) => <FaSuitcase className={className} />,
   HighHeel: (className?: string) => <PiHighHeelFill className={className} />,
   Camera: (className?: string) => <FaCamera className={className} />,
+  Home: (className?: string) => <FaHome className={className} />,
+  Heart: (className?: string) => <FaRegHeart className={className} />,
+  HeartFill: (className?: string) => <FaHeart className={className} />,
+  Activity: (className?: string) => <PiListMagnifyingGlass className={className} />,
+  Notification: (className?: string) => <RiNotification3Line className={className} />,
+  NotificationFill: (className?: string) => <RiNotification3Fill className={className} />,
 };
 
 export default Icons;
