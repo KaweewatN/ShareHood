f317720ed0a673033eb73021579a477c13e5bb1b
Kharukulavat <nipunk0258@gmail.com>
2024-11-10 12:41:32 +0700

    fix: Back button

--- share-hood/src/components/notification/NotificationPage.tsx
+++ share-hood/src/components/notification/NotificationPage.tsx
@@ -2,13 +2,14 @@
 
 import React from 'react';
 import NotificationList from './NotificationList';
+import BackButton from '@components/hood.ui/BackButton'; // Importing BackButton from Shad/UI
 
 const NotificationPage: React.FC = () => {
   return (
     <div className="max-w-md mx-auto p-4">
-      {/* Header */}
+      {/* Header with BackButton */}
       <header className="relative flex items-center justify-center mb-4">
-        <button className="absolute left-0 text-gray-600 text-lg">&larr;</button>
+        <BackButton path="/previous-page" className="absolute left-0" /> {/* Utilizing BackButton from Shad/UI */}
         <h1 className="text-xl font-bold text-gray-800">Notifications</h1>
       </header>
 
