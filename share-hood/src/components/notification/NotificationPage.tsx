d49d83ffba632cf890d298057a03d935b61f6634
Kharukulavat <nipunk0258@gmail.com>
2024-11-10 13:17:36 +0700

    style: home landing page

--- share-hood/src/components/notification/NotificationPage.tsx
+++ share-hood/src/components/notification/NotificationPage.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import React from 'react';
-import NotificationList from './NotificationList';
-import BackButton from '@components/hood.ui/BackButton'; // Importing BackButton from Shad/UI
+import React from "react";
+import NotificationList from "./NotificationList";
+import BackButton from "@components/hood.ui/BackButton"; // Importing BackButton from Shad/UI
 
 const NotificationPage: React.FC = () => {
   return (
-    <div className="max-w-md mx-auto p-4">
+    <div className="mx-auto max-w-md p-4">
       {/* Header with BackButton */}
-      <header className="relative flex items-center justify-center mb-4">
-        <BackButton path="/previous-page" className="absolute left-0" /> {/* Utilizing BackButton from Shad/UI */}
+      <header className="relative mb-4 flex items-center justify-center">
+        <BackButton path="/previous-page" className="absolute left-0" />{" "}
+        {/* Utilizing BackButton from Shad/UI */}
         <h1 className="text-xl font-bold text-gray-800">Notifications</h1>
       </header>
 
