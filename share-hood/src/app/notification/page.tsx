bdde88f178b23d962281f784376900e4eb7988df
TK <klarrrr17@gmail.com>
2024-11-14 03:56:39 +0700

    feat: add authentication to Notification page

--- share-hood/src/app/notification/page.tsx
+++ share-hood/src/app/notification/page.tsx
@@ -4,9 +4,16 @@ import NotificationPage from "@components/notification/NotificationPage";
 import {authenticateUser} from "src/service/functions/NextAuthFunction";
 import {redirect} from "next/navigation";
 
+import Menubar from "@components/hood.ui/MenuBar";
+
 export default async function Notification() {
   await authenticateUser().catch(() => {
     redirect("/authentication");
   });
-  return <NotificationPage />;
+  return (
+    <>
+      <NotificationPage />
+      <Menubar />
+    </>
+  );
 }
