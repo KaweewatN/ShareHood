5018e9fa3922782088936732c0dc0bdb53052ef3
TK <klarrrr17@gmail.com>
2024-11-14 03:47:32 +0700

    fix: Notification function

--- share-hood/src/app/notification/page.tsx
+++ share-hood/src/app/notification/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import NotificationPage from "@components/notification/NotificationPage";
 
-const Page: React.FC = () => {
-  return <NotificationPage />;
-};
+import {authenticateUser} from "src/service/functions/NextAuthFunction";
+import {redirect} from "next/navigation";
 
-export default Page;
+export default async function Notification() {
+  await authenticateUser().catch(() => {
+    redirect("/authentication");
+  });
+  return <NotificationPage />;
+}
